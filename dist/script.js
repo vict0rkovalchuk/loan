e9957f37d63ae7986cd3289977745edd7d394ca3
Victor <vict02r05@gmail.com>
2023-12-05 16:09:22 +0200

    add popup block for third slide

--- dist/script.js
+++ dist/script.js
@@ -26,6 +26,18 @@ class Slider {
     if (n < 1) {
       this.slideIndex = this.slides.length;
     }
+    try {
+      this.hanson.style.opacity = '0';
+      if (n === 3) {
+        this.hanson.classList.add('animated');
+        setTimeout(() => {
+          this.hanson.style.opacity = '1';
+          this.hanson.classList.add('fadeInUp');
+        }, 3000);
+      } else {
+        this.hanson.classList.remove('fadeInUp');
+      }
+    } catch (error) {}
     [...this.slides].forEach(slide => {
       slide.style.display = 'none';
     });
@@ -35,6 +47,9 @@ class Slider {
     this.showSlides(this.slideIndex += n);
   }
   render() {
+    try {
+      this.hanson = document.querySelector('.hanson');
+    } catch (error) {}
     this.btns.forEach(btn => {
       btn.addEventListener('click', () => {
         this.plusSlides(1);
