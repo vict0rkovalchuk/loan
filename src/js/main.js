f9c768aeae2675674b0e20f6ef25ff740534a141
Victor <vict02r05@gmail.com>
2023-12-10 14:29:55 +0200

    implement item 13 of the tech task; appearance of the content

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
 import Form from './modules/forms';
+import ShowInfo from './modules/showInfo';
 
 window.addEventListener('DOMContentLoaded', () => {
   const slider = new MainSlider({ btns: '.next', container: '.page' });
@@ -47,4 +48,5 @@ window.addEventListener('DOMContentLoaded', () => {
   new VideoPlayer('.module__video-item .play', '.overlay').init();
   new Difference('.officerold', '.officernew', '.officer__card-item').init();
   new Form('.form').init();
+  new ShowInfo('.plus').init();
 });
